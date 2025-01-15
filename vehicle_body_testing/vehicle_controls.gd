07f3d04311fc40a9e8295e206832609af4032d2f
joe <cafejoe61@google.com>
2025-01-15 08:16:37 -0600

    tank controls work, but the values are stupid

--- vehicle_body_testing/vehicle_controls.gd
+++ vehicle_body_testing/vehicle_controls.gd
@@ -9,25 +9,30 @@ func _ready():
 func _process(delta):
   pass
 
-@export var pwr: float = 1.0
+@export var engine_force_value: float = 40.0
 @export var rotation_speed : float = 1
 @export var steering_max = deg_to_rad(15)
+@export var turn_speed = 0.5;
+@export var brake_force_max = 30;
 var cmt = false
 func _physics_process(delta):
   if Input.is_action_pressed("fwd"):
-        #stop left side
-    engine_force = pwr
+    var speed := linear_velocity.length()
+    if speed < 5.0 and not is_zero_approx(speed):
+      engine_force = clampf(engine_force_value * 5.0 / speed, 0.0, 100.0)
+    else:
+      engine_force = engine_force_value
   else:
-    engine_force = 0
+    engine_force =  move_toward(engine_force,0,8*delta) 
     
   if Input.is_action_pressed("back"):
-    brake = pwr
+    brake = move_toward ( brake, brake_force_max, 2*delta )
   else:
     brake = 0
     
   if Input.is_action_pressed("left"):
     #stop left side
-    steering = move_toward( steering , -steering_max , 2*delta)
+    steering = move_toward( steering , -steering_max , turn_speed*delta)
     #if(linear_velocity.length() < 5):
       #rotate_y(rotation_speed * delta)
     #else:
@@ -37,16 +42,13 @@ func _physics_process(delta):
       #rotate_y(-rotation_speed * delta)
     #else:
       #steering = steering_max
-    steering = move_toward( steering , steering_max , 2*delta)
+    steering = move_toward( steering , steering_max , turn_speed*delta)
   else:
-    steering = move_toward( steering , 0 , 5*delta)
+    steering = move_toward( steering , 0 , turn_speed/2*delta)
 
     
   if Input.is_action_pressed("exit"):
     get_tree().quit()
-  if Input.is_action_pressed("fwd"):
-    print("Moving @ " + str(pwr) + " force")
-    engine_force = pwr
     
   else:  
     cmt = false
