8eea8400001efd31092e32e04c14f51ade1e1465
joe <cafejoe61@google.com>
2025-01-14 23:47:29 -0600

    vehicle controls experiment

---
+++ tank/tank_controls.gd
@@ -0,0 +1,27 @@
+extends VehicleBody3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+  pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+  pass
+
+@export var pwr: float = 1.0
+
+var cmt = false
+func _physics_process(delta):
+  if Input.is_action_pressed("fwd"):
+    print("Moving")
+    engine_force = pwr
+  if Input.is_action_pressed("exit"):
+    get_tree().quit()
+    
+    
+  else:  
+    cmt = false
+    
+    
